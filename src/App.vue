ef388034fde65a07bde7a4425d647537d2b7c0f1
dezhik74 <dezhik@yandex.ru>
2021-11-01 17:56:46 +0300

    версия 0.1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div id="app">
+    <div class="office">
+      <Map v-on:table-select="currentEmpChanged" />
+      <SideMenu
+        :isUserOpenned="showPersonCard"
+        :person="employees[currentEmpID]"
+        v-on:update:isUserOpenned="currentEmpID = -1"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 import Map from "./components/Map.vue";
 import SideMenu from "./components/SideMenu.vue";
+import people from "./assets/data/people.json";
 
 export default {
   name: "App",
@@ -17,40 +22,68 @@ export default {
     Map,
     SideMenu,
   },
+  data() {
+    return {
+      currentEmpID: -1,
+      employees: [],
+    };
+  },
+  methods: {
+    currentEmpChanged(newPlaceID) {
+      if (newPlaceID === undefined) {
+        this.currentEmpID = -1;
+        return;
+      }
+      let newEmpl = this.employees.find((it) => it.tableId == newPlaceID);
+      if (newEmpl === undefined) {
+        this.currentEmpID = -1;
+      } else {
+        this.currentEmpID = newEmpl._id;
+      }
+    },
+  },
+  computed: {
+    showPersonCard: function () {
+      return this.currentEmpID > -1;
+    },
+  },
+  mounted() {
+    this.employees = people;
+  },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 400px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
